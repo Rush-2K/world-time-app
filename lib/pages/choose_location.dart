bacfc674394e7eb9c1e23f0ea9b85bfe21316385
Rush-2K <syaidiazhari@gmail.com>
2023-07-03 18:36:32 +0800

    import http packages, get dummy data using json placeholder

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,29 +9,7 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-
-    //simulate network request for a data
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'Rush2K';
-    });
-
-    //simulate network request to get bio of username
-    int age = await Future.delayed(Duration(seconds: 2), () {
-      return 23;
-    });
-
-    print ('$username - $age');
-
-  }
-
   int counter = 0;
-
-  @override
-  void initState() {     //run once
-    super.initState();
-    getData();   //since its async, any function/method below can be run without waiting this method to finish
-  }
 
   @override
   Widget build(BuildContext context) {     //run every time the state is change
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+
+    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Map data = jsonDecode(response.body);//convert json string to a Json object so that can easily access the data
+    print(data);
+    print(data['title']);
+
+  }
+
+  @override
+  void initState() {     //run once
+    super.initState();
+    getData();   //since its async, any function/method below can be run without waiting this method to finish
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
