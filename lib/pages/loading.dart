6af9515d78392cb8a81c2ed7513c5ec45d830c70
Rush-2K <syaidiazhari@gmail.com>
2023-07-04 15:02:19 +0800

    use flutter_spinkit packages, make a proper loaders on loading

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -13,11 +14,11 @@ class _LoadingState extends State<Loading> {
   void setupWorldTime() async{
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    Navigator.pushReplacementNamed(context, '/home', arguments: {
-      'location': instance.location,
-      'flag': instance.flag,
-      'time': instance.time,
-    });
+    // Navigator.pushReplacementNamed(context, '/home', arguments: {
+    //   'location': instance.location,
+    //   'flag': instance.flag,
+    //   'time': instance.time,
+    // });
 
   }
 
@@ -30,10 +31,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading'),
-      ),
+      backgroundColor: Colors.purple[700],
+      body: Center(
+        child: SpinKitWave(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      )
     );
   }
 }
